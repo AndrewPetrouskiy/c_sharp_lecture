ec617c6eb47152f33ca95f868a91937f4e9b1e5a
KeNnY <kenny_0@mail.ru>
2022-08-05 19:56:37 +0300

    friends and dog2

--- ex009/Program.cs
+++ ex009/Program.cs
@@ -1,28 +1,27 @@
-﻿int count = 0;
-int time = 0;
-int distance = 10000;
-int first_person_speed = 1;
-int second_person_speed = 2;
-int dog_speed = 5;
-int friend =2;
+﻿double count = 0;
+double time = 0;
+double distance = 10000;
+double first_person_speed = 1;
+double second_person_speed = 2;
+double dog_speed = 5;
+double friend =2;
 
 
-while(distance > 10)
+while(distance > 5)
 {
     if(friend == 1)
     {
         time = distance / (first_person_speed + dog_speed);
         friend = 2;
-        distance = distance - (first_person_speed + second_person_speed) * time;
     }
     
     if(friend == 2)
     {
-        time = distance /(second_person_speed + dog_speed);
+        time = distance / (second_person_speed + dog_speed);
         friend = 1;
-        distance = distance - (first_person_speed + second_person_speed) * time;
     }
     
+    distance = distance - (first_person_speed + second_person_speed) * time;
     count = count + 1;
     Console.WriteLine(count);
 }
